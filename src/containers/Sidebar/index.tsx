a0b7958cd36823dc6d83782e62d070ac72a6eaa0
Desiree <desireerdias@gmail.com>
2025-12-05 10:37:34 -0300

    criando a estrutura do projeto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/desireedias.png" alt="avatar github" />
+    <Titulo>Desiree Dias</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre Mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptate
+      quam molestiae nobis repudiandae eius, fugit corporis doloremque,
+      doloribus quisquam possimus. Commodi est sunt quaerat inventore. Fugiat
+      dolorum magnam perferendis!
+    </p>
+  </section>
+)
+
+export default Sobre
