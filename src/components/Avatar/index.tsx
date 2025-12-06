0310415f393bedcdd424fb96eb04851f8e80f767
Desiree <desireerdias@gmail.com>
2025-12-06 10:11:15 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/desireedias.png" alt="foto perfil" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/desireedias.png" alt="avatar github" />
-    <Titulo fontSize={20}>Desiree Dias</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Desiree Dias</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        desireedias
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Full Stack | Python
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
